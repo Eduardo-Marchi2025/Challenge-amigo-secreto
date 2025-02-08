b0ea17abb52d06e508f1c21ec9da80b3dde331a1
Eduardo Marchi <eduardo.marchi@gmail.com>
2025-02-08 16:07:51 -0300

    Update app.js
    Modificações já realizadas.

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,4 +1,5 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 let sorteioRealizado = false;
 let toRemove = '';
@@ -157,4 +158,3 @@ function limparElemento(elemento) {
 function alterarValorElemento(elemento, valor) {
     document.getElementById(elemento).innerHTML = valor;
 }
-
